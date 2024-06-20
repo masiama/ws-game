f35a71e5e08a52bf0800de750d00527a6bc0edaa
Leonid Cupikov <leonid.cupikov@gmail.com>
2024-06-20 06:36:16 +0200

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,10 @@ import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
 
 export default {
-	input: 'src/client/index.js',
-	output: {
-		file: 'out/client.js',
-		format: 'esm'
-	},
-	plugins: [
-		babel({ babelHelpers: 'bundled' }),
-		terser(),
-	]
+  input: 'src/client/index.js',
+  output: {
+    file: 'out/client.js',
+    format: 'esm',
+  },
+  plugins: [babel({ babelHelpers: 'bundled' }), terser()],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,23 @@ const fs = require('fs');
 
 const nodeModules = {};
 fs.readdirSync('node_modules')
-	.filter(x => ['.bin'].indexOf(x) === -1)
-	.forEach(mod => nodeModules[mod] = 'commonjs ' + mod);
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach(mod => (nodeModules[mod] = 'commonjs ' + mod));
 
 module.exports = {
-	entry: './src/server',
-	target: 'node',
-	output: {
-		path: path.resolve(__dirname, 'out'),
-		filename: 'server.js',
-	},
-	externals: nodeModules,
-	plugins: [new TerserPlugin()],
-	module: {
-		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-		],
-	},
-	optimization: {
-		usedExports: true
-	},
-	mode: 'development',
+  entry: './src/server',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'server.js',
+  },
+  externals: nodeModules,
+  plugins: [new TerserPlugin()],
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+  optimization: {
+    usedExports: true,
+  },
+  mode: 'development',
 };
